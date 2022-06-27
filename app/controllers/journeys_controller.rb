86dd7a1b12cf14caec9cf556933db57d63d8a2bc
Quentin Peltier <quentin.peltier.pro@gmail.com>
2022-06-27 18:52:00 +0200

    feat(calculate-co2): calcul + db insert

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -2,4 +2,33 @@ class JourneysController < ApplicationController
   def index
     @journeys = Journey.all
   end
+
+  def result
+    #kgCO2e
+    @km = params[:km]
+    @transports = Transport.all
+    @i = 0
+    @results = []
+
+    @transports.each do |transport|
+      @results[@i]= {
+        'co2' => @km.to_f * transport.multiplier,
+        'slug' => transport.slug
+      }
+      @i += 1
+    end
+
+    @journey = Journey.new
+    @journey.distance = @km
+    @journey.result_walk = @results[0]['co2']
+    @journey.result_train = @results[1]['co2']
+    @journey.result_electrical_car = @results[2]['co2']
+    @journey.result_bus = @results[3]['co2']
+    @journey.result_plane = @results[4]['co2']
+    @journey.result_car = @results[5]['co2']
+    @journey.user_id = current_user.id
+    @journey.save
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: "home#index"
   get "about", to: "about#index"
   get "calculate", to: "journeys#index", as: "journeys"
+  post "result", to: "journeys#result"
   get "transports", to: "transports#index"
   get "users", to: "users#index"
 end
